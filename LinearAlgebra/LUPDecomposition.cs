099a74a30f537b0864f579bc381368d644c9889c
Henry Luscombe <henry.luscombe@hotmail.co.uk>
2021-01-30 20:48:10 +0000

    Implement basic LUP Decomposition test

--- LinearAlgebra/LUPDecomposition.cs
+++ LinearAlgebra/LUPDecomposition.cs
@@ -6,24 +6,26 @@ namespace LinearAlgebra
 {
     public class LUPDecomposition : MatrixBase<LUPDecomposition>
     {
-        public Matrix LUMatrix;
+        private Matrix luMatrix;
         private int[] PermutationArray;
         public LUPDecomposition(Matrix A)
         {
-            double[,] luArray = new double[A.Shape.Item1, A.Shape.Item2];
-            double[] swapArray = new double[A.Shape.Item2]; 
-            int[] permutationArray = new int[A.Shape.Item1];
-            for (int i = 1; i < A.Shape.Item1; i++)
-            {
-                
-            }
-            LUMatrix = new Matrix(luArray);
+            int shape = A.Shape.Item1;
+            double[,] luArray = new double[shape, shape];
+            double[] swapArray = new double[shape];
+            int[] permutationArray = new int[shape];
+            luMatrix = new Matrix(luArray);
             PermutationArray = permutationArray;
         }
 
         public override double MemoryUsage()
         {
-            return ((double)PermutationArray.Length * 32) + LUMatrix.MemoryUsage();
+            return ((double)PermutationArray.Length * 32) + luMatrix.MemoryUsage();
+        }
+
+        public Matrix LUMatrix
+        {
+            get => luMatrix;
         }
 
         public override bool Equals(LUPDecomposition other)
@@ -32,3 +34,4 @@ namespace LinearAlgebra
         }
     }
 }
+
---
+++ LinearAlgebraTests/LUPDecompositionTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using LinearAlgebra;
+
+namespace LinearAlgebraTests
+{
+    class TestLUPDecompositionTests
+    {
+        [Test]
+        public void TestLUPDecompose()
+        {
+            Matrix input = new Matrix(new double[,] { { 5, 3, 3 }, { 10, 6, 3 }, { 5, 6, 3 } });
+            LUPDecomposition output = new LUPDecomposition(input);
+            Assert.True(output.LUMatrix.Equals(new Matrix(new double[,] { { 10, 6, 3 }, { 0, 3, 1.5 }, { 0, 0, 1.5 } })));
+
+        }
+    }
+}
+
